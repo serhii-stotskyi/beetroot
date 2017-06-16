e525ac84f18331f541b48e3e2ef562f21adbf9d8
silaunosti <Sergey13>
2017-06-16 20:06:23 +0300

    init commit

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<?php
+$a = '1';
+//var_dump('$a', $a);
+$b = "variable";
+//var_dump('$b',$b); die;
+$c = 1.1;
+$d = true;
+$e = false;
+$monts = array ('January', 'February'); // php under 5.6
+$arr = [$b => 'this','five' => 5,3];// php upper 5.6
+$variable = 'hi';
+$$variable = 'world'; // $hi = 'world';
+$d = NULL;
+$var1 = (bool)(0);
+?>
