be35aae77ce8abbe4843d7934315bb0ca5b34be0
nikitaserduk <serduk.n.89@gmail.com>
2017-07-28 19:11:44 +0300

    Refactored calculating price

--- www/task8/php/count.php
+++ www/task8/php/count.php
@@ -6,54 +6,36 @@ $promo = $_POST['promo'];
 $response = [];
 
 /**
- *
- * @param $cardName
  * @param $cardCount
+ * @param $cardName
+ * @param int $sale
  * @return mixed
  */
-function getSum($cardName,$cardCount){
-    $sql_1="SELECT `price` FROM `product` WHERE  `product_name` =  '$cardName'";
+function getOrderPrice($cardCount, $cardName, $sale = 0)
+{
     global $connect;
-    $price_sql = mysqli_query($connect, $sql_1);
-    $price = mysqli_fetch_assoc($price_sql);
-    return $cardCount * $price['price'];
+
+    $getPriceSql = "SELECT `price` FROM `product` WHERE  `product_name` =  '$cardName'";
+    $orderPrice = mysqli_fetch_assoc(mysqli_query($connect, $getPriceSql));
+    return ($cardCount * $orderPrice['price']) * (100 - $sale['sale'])/100;
 }
 
-if(!empty($promo)){
-    $sql="SELECT * FROM `promo_code`  WHERE  `code` =  '$promo'";
+if (!empty($promo)) {
+    $sql = "SELECT sale FROM  `promo_code` WHERE  `code` =  '$promo' 
+            AND  `is_active` =  '1'";
     $promo_sql = mysqli_query($connect, $sql);
-    if (mysqli_num_rows($promo_sql) != 0) {
-        $sql_3="SELECT status FROM `promo_code`  WHERE  `code` =  '$promo'";
-        $status_sql = mysqli_query($connect, $sql_3);
-        $arr = mysqli_fetch_assoc($status_sql);
-        if ($arr['status']==='enable'){
-            $sql_4="SELECT sale FROM `promo_code`  WHERE  `code` =  '$promo'";
-            $sale_sql = mysqli_query($connect, $sql_4);
-            $sale = mysqli_fetch_assoc($sale_sql);
-            $sql_5="SELECT `price` FROM `product` WHERE  `product_name` =  '$cardName'";
-            $price_sql = mysqli_query($connect, $sql_5);
-            $price = mysqli_fetch_assoc($price_sql);
-            $sale_1 = (($cardCount * $price['price'])/100)*$sale['sale'];
-            $sum = ($cardCount * $price['price'])-$sale_1;
-            $response['sum'] = $sum;
-
-
-        }else{
-            $sum = getSum($cardName,$cardCount);
-            $response['sum'] = $sum;
-            $response['message'] ="Promotional code is not valid";
-        }
-    } else{
-        $sum = getSum($cardName,$cardCount);
-        $response['sum'] = $sum;
-        $response['message'] ="Promo code is not correct";
-
+    $sale = mysqli_fetch_assoc($promo_sql);
+    if ($sale['sale']) {
+        $sum = getOrderPrice($cardCount, $cardName, $sale['sale']);
+        $response['message'] = "Promo code successfully applied, sale is " . $sale['sale'];
+    } else {
+        $sum = getOrderPrice($cardCount, $cardName);
+        $response['message'] = "Promo code is not correct ";
     }
-}else {
-    $sum = getSum($cardName,$cardCount);
-    $response['sum'] = $sum;
-
-
+} else {
+    $sum = getOrderPrice($cardCount, $cardName);
 }
-echo  json_encode($response);
+
+$response['sum'] = $sum;
+echo json_encode($response);
 exit();
